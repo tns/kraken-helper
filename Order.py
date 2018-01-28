e3c131ac7ac6aa48980a7acc2d755476bdc81fc5
tns <thomas@scheuringer.net>
2018-01-28 02:05:17 +0100

    some new stuff

--- Order.py
+++ Order.py
@@ -15,6 +15,7 @@ class Order:
         self._volume = 0.0
         self._volume_exec = 0.0
         self._type = ''
+        self._pair= ''
         self._description = ''
         self.update()
     
@@ -32,14 +33,29 @@ class Order:
         else:
             self._status = self._ret['result'][self._txid]['status']
             self._description = self._ret['result'][self._txid]['descr']['order']
+            self._type = self._ret['result'][self._txid]['descr']['type']
+            self._pair = self._ret['result'][self._txid]['descr']['pair']
             self._volume = decimal.Decimal(self._ret['result'][self._txid]['vol'])
             self._volume_exec = decimal.Decimal(self._ret['result'][self._txid]['vol_exec'])
+
+    def cancel(self):
+        try:
+            cret = self._kApi.query_private('CancelOrder',
+                                            {'txid': self._txid})
+        except (http.client.HTTPException) as e:
+            print(e.args[0])
+
+        if (self._ret['error'] != []):
+            print(str(self._ret['error']))
 
     def getStatus(self):
         return self._status
 
     def getTxid(self):
         return self._txid 
+
+    def getType(self):
+        return self._type
 
     def getVolume(self):
         return self._volume
@@ -68,3 +84,11 @@ class Order:
         else:
             return False
 
+    def isCanceled(self):
+        if self._status == 'canceled':
+            return True
+        else:
+            return False
+
+    def getPair(self):
+        return self._pair
